5e5fd090ba64866ff44b20bfe89aec17b61d946f
greeenOrange <gm1sadiq@gmail.com>
2025-01-20 12:59:01 +0600

    services section added

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+    $('.image-popup').magnificPopup({
+        type: 'image'
+        // other options
+      });
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+        // other options
+      });
