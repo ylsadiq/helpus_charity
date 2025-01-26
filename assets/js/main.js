e1ed3382f77936d2076bda1cf5f77cadadd892d8
greeenOrange <gm1sadiq@gmail.com>
2025-01-26 15:12:04 +0600

    about us

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
-    $('.image-popup').magnificPopup({
-        type: 'image'
-        // other options
-      });
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-        // other options
-      });
+(function ($) {
+  "use strict";
+
+  $(".image-popup").magnificPopup({
+    type: "image",
+    // other options
+  });
+  $(".video-popup").magnificPopup({
+    type: "iframe",
+    // other options
+  });
+})(jQuery);
